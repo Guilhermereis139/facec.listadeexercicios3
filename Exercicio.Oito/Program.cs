4e5f998a6764152a4dbab6289d01dc0329f784ec
guilherme reis <guilhermereis139@gmail.com>
2022-03-25 19:22:14 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio.Oito/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercicio.Oito
+{
+    class Program
+
+    {
+        private static readonly char[] _vogais = new char[]
+        {
+            'a', 'e', 'i', 'o','u'
+        };
+       // private const char _a = 'a';
+       // private const char _e = 'e';
+       //private const char _i = 'i';
+        //private const char _o = 'o';
+        //private const char _u = 'u';
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+
+                Console.WriteLine("Informe o nome:");
+                var nome = Console.ReadLine().ToLower();
+                var count = 0;
+
+
+                // for (int i = 0; i < nome.Length; i++)
+                foreach (var Letra in nome)
+                {
+                   // var letra = nome[i];
+
+                    // if (_a.Equals(letra)
+
+                    //  && _e.Equals(nome[letra])
+                    // && _i.Equals(nome[letra])
+                    // && _o.Equals(nome[letra])
+                    // && _u.Equals(nome[letra]))
+                    if (!_vogais.Contains(Letra))
+                        count++;
+                }
+                Console.WriteLine($"Seu nome possui {count} Consoantes!");
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ aula 23-03-2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula_23_03_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
